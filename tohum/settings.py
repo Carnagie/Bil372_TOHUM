455b689367d59cb7638fae552d4b36c75851c9a2
ardaekinci <ekinci-arda@outlook.com>
2020-10-17 17:17:00 +0300

    postgreSQL integration

--- tohum/settings.py
+++ tohum/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tohum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tohum',
+        'USER': 'tohumadmin',
+        'PASSWORD': 'whqrnr&6mxAj7',
+        'HOST': 'Carnagie-1760.postgres.pythonanywhere-services.com',
+        'PORT': '11760',
     }
 }
 
