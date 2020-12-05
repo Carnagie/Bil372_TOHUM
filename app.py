132326840123c26b463a496c22bde98031718300
Carnagie <ahmetbahadireyuboglu@gmail.com>
2020-12-05 19:23:18 +0300

    first flask template init

--- app.py
+++ app.py
@@ -5,17 +5,27 @@ app = Flask(__name__)
 app.secret_key = "hello"
 #con = psycopg2.connect(host="localhost", port="5432", database="tohum", user="postgres", password="147369")
 
-@app.route('/')
-def homepage():
-    return "deneme"
+
+
+
+
+@app.route('/', methods=["GET","POST"])
+def index():
+
+    return render_template('index.html')
+
+
 
 @app.route('/login',methods=["POST","GET"])
 def login():
-    pass
+
+    return render_template('login.html')
 
 @app.route('/register', methods=["POST","GET"])
 def register():
-    pass
+    
+
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
