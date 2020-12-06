8003525b261e43e39ac040fe9e2c680c65d5e53e
ardaekinci <ekinci-arda@outlook.com>
2020-12-07 01:04:46 +0300

    product insert

--- database/insert.py
+++ database/insert.py
@@ -3,10 +3,27 @@ import psycopg2
 from csv import reader
 
 
-con = psycopg2.connect(host="localhost", port="5432", database="tohumdb", user="postgres", password="facethest0rm")
+con = psycopg2.connect(host="localhost", port="5432", database="Tohum", user="postgres", password="Sarris5599")
 #con = psycopg2.connect(host="localhost", port="9999", database="tohumdb", user="super", password="whqrnr&6mxAj7")
 
 cur = con.cursor()
+
+with open('product.csv', 'r', encoding="UTF8") as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    # Iterate over each row in the csv using reader object
+    next(csv_reader)
+    for row in csv_reader:
+        # row variable is a list that represents a row in csv
+        print(row)
+        print("INSERT INTO tohumschema.product VALUES ( " + row[0] + "," + row[1] + "," + row[2] + ")")
+        sql_insert = "INSERT INTO tohumschema.product (name, type, coefficient, regionid) VALUES ( '" + row[0] + "'," + row[1] + "," + row[2]  +"," + row[3]  +");"
+
+        cur.execute(sql_insert)
+cur.close()
+con.commit()
+con.close()
+
 
 """
 with open('state.csv', 'r') as read_obj:
@@ -43,4 +60,5 @@ with open('city.csv', 'r') as read_obj:
 cur.close()
 con.commit()
 con.close()
+
 """
