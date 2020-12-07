4964b7d91b96a39d2d876ea97b43dfc0fb34c88b
ardaekinci <ekinci-arda@outlook.com>
2020-12-07 15:55:22 +0300

    farmer data script.

---
+++ database/farmer.sql
@@ -0,0 +1,44 @@
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('ali@hotmail.com','ali','durmaz','alidurmaz',43);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('ahmet@gmail.com','ahmet','yılmaz','ahmetyilmaz',32);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('velican@outlook.com','veli','can','velican',57);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('cem@gmail.com','cem','susmaz','cemsusmaz',13);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('hazan@hotmail.com','hazan','sayan','hazansayan',06);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('tolga@outlook.com','tolga','güneş','tolgagunes',81);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('sule@gmail.com','şule','hızlı','sulehizli',61);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('tamergun@hotmail.com','tamer','gün','tamergun',33);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('sevde@outlook.com','sevde','kara','sevdekara',23);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('naci@gmail.com','naci','saygın','nacisaygın',43);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('gulocak@hotmail.com','gül','ocak','ocak',75);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('selen@outlook.com','selen','mart','selenmart',28);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('gokmen@hotmail.com','gokmen','gündoğdu','gokmengundogdu',39);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('mustafaali@gmail.com','mustafa','ali','mustafaali',19);
+
+INSERT INTO tohumschema.farmer (mail,name,lastname,password,city)
+VALUES ('baris@hotmail.com','barış','kahraman','bariskahraman',57);
